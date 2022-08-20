6cf2641f804689537472381a2e347d02a901b329
Kiran1689 <75929997+Kiran1689@users.noreply.github.com>
2022-08-20 10:03:09 +0530

    Add files via upload

---
+++ Voting_System/client/src/component/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Voting System
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
